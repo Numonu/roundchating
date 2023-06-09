5335b974efb94e74b85dc9c92a0dd39c44b96280
Numonu <juan.villegas.dev@gmail.com>
2023-06-09 12:53:34 -0500

    (build):project setup

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./global/styles.css";
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h1 className='text-emerald-500 text-6xl font-medium'>Project Setup</h1>
+  </React.StrictMode>,
+)
