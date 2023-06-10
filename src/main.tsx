e2d6b96f5b9ef7ef3d0adc7f4ca0bffdf24690fe
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 12:28:11 -0500

    (feat):import App in main script

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "./global/styles.css";
+import App from './layout/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1 className='text-emerald-500 text-6xl font-medium'>Project Setup</h1>
+    <App/>
   </React.StrictMode>,
 )
