e25ca367fde38d185d7968b29f0e8afd2a97f5af
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 12:26:54 -0500

    (style):format Chat layout

---
+++ src/layout/Chat.tsx
@@ -0,0 +1,15 @@
+export default function Chat() {
+	return (
+		<div className="bg-emerald-500 grid grid-rows-[min-content_1fr_min-content] overflow-hidden">
+			<div className="bg-red-500">
+                Search
+            </div>
+			<div className="bg-sky-500">
+				Chat Aside
+			</div>
+			<div className="bg-indigo-700">
+                Input
+            </div>
+		</div>
+	);
+}
