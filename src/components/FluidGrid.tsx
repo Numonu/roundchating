98bee950b1c6b787debc28df739c3aa7fe610715
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 17:05:31 -0500

    (update):move fluid grid to components directory

---
+++ src/components/FluidGrid.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import Chat from "../layout/Chat";
+import Rooms from "../layout/Rooms";
+import User from "../layout/User";
+import { ISceneContextProps, SceneContext } from "../context/SceneProvider";
+
+export default function FluidGrid() {
+	const { classHandler } = useContext(SceneContext) as ISceneContextProps;
+	return (
+		<div
+			className={`grid transition-[grid-template-columns] ${classHandler}`}
+		>
+			<User />
+			<Rooms />
+			<Chat />
+		</div>
+	);
+}
