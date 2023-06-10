35a409fd7a6d6362c409661f93e7707d26d7b054
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 13:39:26 -0500

    (feat):create context for scene data

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,40 +1,49 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import Chat from "./Chat";
 import Rooms from "./Rooms";
 import User from "./User";
 import Header from "./Header";
 
-enum display{
-    CHAT,
-    ROOMS,
-    USER
+enum Display {
+	CHAT,
+	ROOMS,
+	USER,
 }
+interface ISceneContextProps {
+    setScene: React.Dispatch<React.SetStateAction<Display>>;
+    scene: Display;
+}
+const SceneContext = createContext<ISceneContextProps | null>(null);
 export default function App() {
-    const [viewRooms , setViewRooms] = useState<display>(display.CHAT);
-    //
-    let classHandler;
-    switch (viewRooms) {
-        case display.USER:
-            classHandler = "grid-cols-[1fr_0fr_0fr]";
-            break;
-        case display.ROOMS:
-            classHandler = "grid-cols-[0fr_1fr_0fr]";
-            break;
-        case display.CHAT:
-            classHandler = "grid-cols-[0fr_0fr_1fr]";
-            break;
-        default:
-            break;
-    }
-    //
+	const [scene, setScene] = useState<Display>(Display.CHAT);
+	//
+	let classHandler;
+	switch (scene) {
+		case Display.USER:
+			classHandler = "grid-cols-[1fr_0fr_0fr]";
+			break;
+		case Display.ROOMS:
+			classHandler = "grid-cols-[0fr_1fr_0fr]";
+			break;
+		case Display.CHAT:
+			classHandler = "grid-cols-[0fr_0fr_1fr]";
+			break;
+		default:
+			break;
+	}
+	//
 	return (
-		<div className="h-screen grid grid-rows-[min-content_1fr]">
-			<Header/>
-			<div className={`grid transition-[grid-template-columns] ${classHandler}`}>
-                <User/>
-				<Rooms/>
-				<Chat/>
+		<SceneContext.Provider value={{ setScene, scene }}>
+			<div className="h-screen grid grid-rows-[min-content_1fr]">
+				<Header />
+				<div
+					className={`grid transition-[grid-template-columns] ${classHandler}`}
+				>
+					<User />
+					<Rooms />
+					<Chat />
+				</div>
 			</div>
-		</div>
+		</SceneContext.Provider>
 	);
 }
