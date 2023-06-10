07087cea196f10bb0a082ff6c99aefe1ffd7cba4
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 17:08:13 -0500

    (update):update components and imports

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,8 +1,9 @@
 import Header from "./Header";
 import SceneProvider from "../context/SceneProvider";
-import FluidGrid from "../components/organisms/FluidGrid";
+import FluidGrid from "../components/FluidGrid";
 
 export default function App() {
+   
 	return (
 		<div className="h-screen grid grid-rows-[min-content_1fr]">
 			<SceneProvider>
--- src/layout/Chat.tsx
+++ src/layout/Chat.tsx
@@ -1,15 +1,13 @@
+import ChatBody from "../components/ChatBody";
+import ChatWriter from "../components/ChatWriter";
+
 export default function Chat() {
+	
 	return (
 		<div className="bg-emerald-500 grid grid-rows-[min-content_1fr_min-content] overflow-hidden">
-			<div className="bg-red-500">
-                Search
-            </div>
-			<div className="bg-sky-500">
-				Chat Aside
-			</div>
-			<div className="bg-indigo-700">
-                Input
-            </div>
+			<div className="bg-red-500">Search</div>
+			<ChatBody/>
+			<ChatWriter/>
 		</div>
 	);
 }
