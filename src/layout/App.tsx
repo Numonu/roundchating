2309000ad1e4dff97400ac0d11b4dac18cd3b763
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 12:23:41 -0500

    (feat):create Chat layout

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Chat from "./Chat";
+import Rooms from "./Rooms";
+import User from "./Users";
 
 enum display{
     CHAT,
@@ -6,7 +9,7 @@ enum display{
     USER
 }
 export default function App() {
-    const [viewRooms , setViewRooms] = useState<display>(display.USER);
+    const [viewRooms , setViewRooms] = useState<display>(display.CHAT);
     //
     let classHandler;
     switch (viewRooms) {
@@ -27,27 +30,9 @@ export default function App() {
 		<div className="h-screen grid grid-rows-[min-content_1fr]">
 			<header className="bg-emerald-500">Header</header>
 			<div className={`grid transition-[grid-template-columns] ${classHandler}`}>
-				<div className="bg-blue-500 overflow-hidden">
-                    Profile Aside
-                    <br />
-                    <button onClick={() => setViewRooms(display.ROOMS)}>Show Rooms</button>
-                </div>
-				<div className="bg-pink-500 overflow-hidden">
-                    Rooms Aside
-                    <br />
-                    <button onClick={() => setViewRooms(display.USER)}>Show User</button>
-                    <br />
-                    <button onClick={() => setViewRooms(display.CHAT)}>Show Chat</button>
-                </div>
-				<div className="bg-emerald-500 grid grid-rows-[min-content_1fr_min-content] overflow-hidden">
-					<div className="bg-red-500">Search</div>
-					<div className="bg-sky-500">
-                        Chat Aside
-                        <br />
-                        <button onClick={() => setViewRooms(display.ROOMS)}>Show Rooms</button>
-                    </div>
-					<div className="bg-indigo-700">Input</div>
-				</div>
+                <User/>
+				<Rooms/>
+				<Chat/>
 			</div>
 		</div>
 	);
