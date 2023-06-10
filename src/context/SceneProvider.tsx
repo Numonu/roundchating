626c51127850d823aed2b52927e94a0f0b8acb75
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 16:20:43 -0500

    (feat):create scene provider for provide layaout properties

---
+++ src/context/SceneProvider.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, createContext, useState } from "react";
+
+export enum Display {
+	CHAT = "chat",
+	ROOMS = "rooms",
+	USER = "user",
+}
+export interface ISceneContextProps {
+    setScene: React.Dispatch<React.SetStateAction<Display>>;
+    classHandler:string;
+    scene: Display;
+}
+export const SceneContext = createContext<ISceneContextProps | null>(null);
+
+export default function SceneProvider({children}:{children:ReactNode}){
+    const [scene, setScene] = useState<Display>(Display.CHAT);
+    //
+	let classHandler;
+	switch (scene) {
+		case Display.USER:
+			classHandler = "grid-cols-[1fr_0fr_0fr] md:grid-cols-[1fr_0fr_2fr] xl:grid-cols-[1.5fr_1.5fr_3fr]";
+			break;
+		case Display.ROOMS:
+			classHandler = "grid-cols-[0fr_1fr_0fr] md:grid-cols-[0fr_1fr_2fr] xl:grid-cols-[1.5fr_1.5fr_3fr]";
+			break;
+		case Display.CHAT:
+			classHandler = "grid-cols-[0fr_0fr_1fr] md:grid-cols-[0fr_0fr_2fr] xl:grid-cols-[1.5fr_1.5fr_3fr]";
+			break;
+		default:
+            classHandler = "grid-cols-[0fr_0fr_1fr]";
+			break;
+	}
+	//
+    return (
+        <SceneContext.Provider value={{ setScene, scene , classHandler }}>
+			{children}
+		</SceneContext.Provider>
+    )
+}
