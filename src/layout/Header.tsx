4697440b1b4effa7f9a573093b2a1ad1d26d8ce2
Numonu <juan.villegas.dev@gmail.com>
2023-06-10 13:39:48 -0500

    (feat):clear innecesary element in Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,10 +3,10 @@ import { FiMoreVertical } from "react-icons/fi";
 export default function Header() {
 	return (
 		<header className="bg-sky-400 p-2 flex justify-between">
-			<div className="flex items-center gap-2 text-xl">
+			<button className="flex items-center gap-2 text-xl cursor-pointer">
 				<BiLeftArrow/>
 				<span>Rooms</span>
-			</div>
+			</button>
 			<div className="flex items-center text-2xl">
 				<FiMoreVertical />
 			</div>
