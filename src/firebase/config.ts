b1953c7991e22d06fa571a78c9ab29ff4a0bd04d
Numonu <juan.villegas.dev@gmail.com>
2023-06-11 16:53:27 -0500

    (fix):replace import firestore lite for normal firestore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 //
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
