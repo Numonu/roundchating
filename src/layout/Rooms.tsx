2cfb9ae30b826544afeec8c66cf02458e451f96d
Numonu <juan.villegas.dev@gmail.com>
2023-06-11 17:39:57 -0500

    (feat):display room avalibles and allow connection for all

--- src/layout/Rooms.tsx
+++ src/layout/Rooms.tsx
@@ -1,7 +1,27 @@
+import { IRoomContextProps, RoomContext } from "../context/RoomProvider";
+import { useContext } from "react";
+
+const roomList = [
+	"Global",
+	"Universal",
+	"Local",
+]
 export default function Rooms() {
+	const {setRoom} = useContext(RoomContext) as IRoomContextProps;
 	return (
 		<div className="bg-pink-500 overflow-hidden">
-			Rooms Aside	
+			<div>
+				<input type="text" placeholder="Searh Some Room"/>
+			</div>
+			<div className="flex flex-col">
+				{
+					roomList.map(e => {
+						return <button onClick={() => setRoom(e)}>
+							{e}
+						</button>
+					})
+				}
+			</div>
 		</div>
 	);
 }
