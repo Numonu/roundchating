d5d6d0a0457356f986c618e8ca98e41c631c33ad
Numonu <juan.villegas.dev@gmail.com>
2023-06-11 16:38:12 -0500

    (feat):chat body read and display messages for db

--- src/components/ChatBody.tsx
+++ src/components/ChatBody.tsx
@@ -1,7 +1,31 @@
-export default function ChatBody(){
-    return (
-        <div>
-            
-        </div>
-    )
+import { collection, getDocs, orderBy, query } from "firebase/firestore/lite";
+import { db } from "../firebase/config";
+import { useEffect, useState } from "react";
+
+type Message = {
+	owner : string,
+	message : string,
+	timestamp : string
+}
+export default function ChatBody() {
+	const [messages , setMessages] = useState<Message[]>([]);
+	useEffect(() => {
+		const q = query(collection(db, "Global"), orderBy("timestamp", "asc"));
+		const result:Message[] = [];
+		getDocs(q).then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				result.push(doc.data() as Message);
+			});
+			setMessages(result);
+		});
+	}, []);
+	return <div>
+		{
+			messages.map(e => {
+				return <div>
+					<h1>{e.message}</h1>
+				</div>
+			})
+		}
+	</div>;
 }
