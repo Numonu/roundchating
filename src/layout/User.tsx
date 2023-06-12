0592538e449468552b86c0d2073500d13a6034c8
Numonu <juan.villegas.dev@gmail.com>
2023-06-12 15:47:38 -0500

    (update):import conditional components

--- src/layout/User.tsx
+++ src/layout/User.tsx
@@ -1,12 +1,17 @@
 import UserBoard from "../components/user/UserBoard";
 import UserForms from "../components/user/UserForms";
+import UserNav from "../components/user/UserNav";
+import { IUserContextProps, UserContext } from "../context/UserProvider";
+import { useContext } from "react";
 
 export default function User() {
+	const { user } = useContext(UserContext) as IUserContextProps;
+
 	return (
 		<div className="bg-neutral-50 overflow-hidden">
 			<div className="px-4">
-				<UserBoard/>
-				<UserForms/>
+				<UserBoard />
+				{user ? <UserNav /> : <UserForms />}
 			</div>
 		</div>
 	);
