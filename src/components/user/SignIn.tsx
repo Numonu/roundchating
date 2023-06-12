9df0ff4c1fec16e88a0c6df324daadbb9276fa09
Numonu <juan.villegas.dev@gmail.com>
2023-06-12 17:57:35 -0500

    (update):replace static content for reusable components

--- src/components/user/SignIn.tsx
+++ src/components/user/SignIn.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { MdAlternateEmail } from "react-icons/md";
+import { BsShieldLock } from "react-icons/bs";
 import { auth } from "../../firebase/config";
+import { useState } from "react";
+import Input from "../Input";
+import { FaPaperPlane } from "react-icons/fa";
+import FluidButton from "./FluidButton";
+
 export default function SignIn() {
 	const [email, setEmail] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
+
 	const signin = async () => {
 		try {
 			await signInWithEmailAndPassword(auth, email, password);
@@ -16,29 +23,17 @@ export default function SignIn() {
 
 	return (
 		<form
-			className="overflow-hidden flex flex-col"
+			className="overflow-hidden flex flex-col gap-4"
 			onSubmit={(e) => {
 				e.preventDefault();
 				signin();
 			}}
 		>
-			<h1>Sign In</h1>
-            <p className="">
-                Sign in and start chating!
-            </p>
-			<input
-				required
-				type="email"
-				placeholder="email"
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<input
-				required
-				type="password"
-				placeholder="password"
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<button>Submit</button>
+            <Input icon={<MdAlternateEmail/>} id="signin-email" type="email" placeholder="email" onChange={e => setEmail(e)}/>
+            <Input icon={<BsShieldLock/>} id="signin-password" type="password" placeholder="password" onChange={e => setPassword(e)}/>
+            <FluidButton icon={<FaPaperPlane/>}>
+                Send
+            </FluidButton>
 		</form>
 	);
 }
--- src/components/user/SignUp.tsx
+++ src/components/user/SignUp.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase/config";
+import { MdAlternateEmail } from "react-icons/md";
+import { BsShieldLock } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
+import Input from "../Input";
+import { FaPaperPlane } from "react-icons/fa";
+import FluidButton from "./FluidButton";
 export default function SignUp() {
+
 	const [email, setEmail] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
+
 	const signup = async () => {
 		try {
 			await createUserWithEmailAndPassword(auth, email, password);
@@ -16,26 +24,18 @@ export default function SignUp() {
 
 	return (
 		<form
-			className="overflow-hidden flex flex-col"
+			className="overflow-hidden flex flex-col gap-4"
 			onSubmit={(e) => {
 				e.preventDefault();
 				signup();
 			}}
 		>
-			<h1>Sign Up</h1>
-			<input
-				required
-				type="email"
-				placeholder="email"
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<input
-				required
-				type="password"
-				placeholder="password"
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<button>Submit</button>
+            <Input icon={<AiOutlineUser/>} id="signup-username" type="text" placeholder="username" onChange={e => setEmail(e)}/>
+			<Input icon={<MdAlternateEmail/>} id="signup-email" type="email" placeholder="email" onChange={e => setEmail(e)}/>
+            <Input icon={<BsShieldLock/>} id="signup-password" type="password" placeholder="password" onChange={e => setPassword(e)}/>
+			<FluidButton icon={<FaPaperPlane/>}>
+                Send
+            </FluidButton>
 		</form>
 	);
 }
