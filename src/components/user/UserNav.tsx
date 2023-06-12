f0937acf40a6c045b6bb49b4435a6fd5a9576ef2
Numonu <juan.villegas.dev@gmail.com>
2023-06-12 17:10:31 -0500

    (design):design navigation buttons

--- src/components/user/UserNav.tsx
+++ src/components/user/UserNav.tsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
+import { FaPaperPlane } from "react-icons/fa";
+import { RiLogoutBoxLine } from "react-icons/ri";
+
 import {
 	Display,
 	ISceneContextProps,
@@ -20,9 +23,21 @@ export default function UserNav() {
 		}
 	};
 	return (
-		<div className="flex flex-col">
-			<button onClick={goToRooms}>View Rooms</button>
-			<button onClick={closeAccount}>Close Account</button>
+		<div className="flex flex-col gap-4">
+			<button
+				className="bg-indigo-600 py-2 rounded-md flex justify-center items-center gap-2"
+				onClick={goToRooms}
+			>
+				<FaPaperPlane className="text-base"/>
+				<span>View Rooms</span>
+			</button>
+			<button
+				className="bg-neutral-300 py-2 rounded-md flex justify-center items-center gap-2"
+				onClick={closeAccount}
+			>
+                <RiLogoutBoxLine className="text-xl"/>
+				<span>Close Account</span>
+			</button>
 		</div>
 	);
 }
