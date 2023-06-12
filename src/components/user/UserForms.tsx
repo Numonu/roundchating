cf138e51effcee2b5a8c9b293c1cceb2917fca8b
Numonu <juan.villegas.dev@gmail.com>
2023-06-12 12:58:34 -0500

    (update):import components for auth form

--- src/components/user/UserForms.tsx
+++ src/components/user/UserForms.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
 const enum Display {
 	SIGNIN,
 	SIGNUP,
@@ -24,36 +26,26 @@ export default function UserForms() {
 	}
 	return (
 		<>
-			<div className="relative grid grid-cols-2 text-center text-xl capitalize">
+			<div className="relative grid grid-cols-2 text-center text-lg capitalize">
 				<div
 					className={`bg-sky-500 absolute w-1/2 h-[5%] bottom-0 transition-transform ${classHandler.guider}`}
 				></div>
 				<button
-					className="bg-white bg-opacity-50 w-full py-3"
+					className="bg-white bg-opacity-50 w-full py-2"
 					onClick={() => setDisplay(Display.SIGNIN)}
 				>
 					<h2>Sign in</h2>
 				</button>
 				<button
-					className="bg-white bg-opacity-50 w-full py-3"
+					className="bg-white bg-opacity-50 w-full py-2"
 					onClick={() => setDisplay(Display.SIGNUP)}
 				>
 					<h2>Sign up</h2>
 				</button>
 			</div>
-            <div className={`p-4 grid grid-flow-col transition-[grid-template-columns] ${classHandler.form}`}>
-                <form className="overflow-hidden flex flex-col">
-                    <h1>Register Now</h1>
-                    <input type="text" placeholder="email" />
-                    <input type="text" placeholder="password" />
-                    <button>Submit</button>
-                </form>
-                <form className="overflow-hidden flex flex-col">
-                    <h1>Register Now</h1>
-                    <input type="text" placeholder="email" />
-                    <input type="text" placeholder="password" />
-                    <button>Submit</button>
-                </form>
+            <div className={`py-4 grid grid-flow-col transition-[grid-template-columns] ${classHandler.form}`}>
+                <SignIn/>
+                <SignUp/>
             </div>
 		</>
 	);
