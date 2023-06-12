fd2ce45fcb1e467ffc43b846b10a0b8ae42633d4
Numonu <juan.villegas.dev@gmail.com>
2023-06-12 16:01:13 -0500

    (feat):select room and changue scene

--- src/layout/Rooms.tsx
+++ src/layout/Rooms.tsx
@@ -1,26 +1,30 @@
 import { IRoomContextProps, RoomContext } from "../context/RoomProvider";
 import { useContext } from "react";
+import {
+	Display,
+	ISceneContextProps,
+	SceneContext,
+} from "../context/SceneProvider";
 
-const roomList = [
-	"Global",
-	"Universal",
-	"Local",
-]
+const roomList = ["Global", "Universal", "Local"];
 export default function Rooms() {
-	const {setRoom} = useContext(RoomContext) as IRoomContextProps;
+	const { setRoom } = useContext(RoomContext) as IRoomContextProps;
+	const { setScene } = useContext(SceneContext) as ISceneContextProps;
+
+	const goToRoom = (e: string) => {
+		setRoom(e);
+		setScene(Display.CHAT);
+	};
+
 	return (
 		<div className="bg-pink-500 overflow-hidden">
 			<div>
-				<input type="text" placeholder="Searh Some Room"/>
+				<input type="text" placeholder="Searh Some Room" />
 			</div>
 			<div className="flex flex-col">
-				{
-					roomList.map(e => {
-						return <button onClick={() => setRoom(e)}>
-							{e}
-						</button>
-					})
-				}
+				{roomList.map((e , i) => {
+					return <button key={i} onClick={() => goToRoom(e)}>{e}</button>;
+				})}
 			</div>
 		</div>
 	);
