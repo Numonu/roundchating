51ef1b491b9f6ac2f8a8776f62574b53e42df02c
Numonu <juan.villegas.dev@gmail.com>
2023-06-13 10:25:26 -0500

    (feat):add disabled btn state

--- src/components/user/FluidButton.tsx
+++ src/components/user/FluidButton.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from "react";
 interface IFluidButtonProps {
+	disabled? : boolean;
 	type?: "button" | "submit";
 	icon?: ReactNode;
 	children: ReactNode;
 }
 export default function FluidButton({
+	disabled,
 	icon,
 	type,
 	children,
 }: IFluidButtonProps) {
 	return (
 		<button
-			className="bg-indigo-600 py-2 rounded-md flex justify-center items-center gap-2"
+			className="bg-indigo-600 py-2 rounded-md flex justify-center items-center gap-2 disabled:opacity-50"
+			disabled={disabled || false}
 			type={type ?? "submit"}
 		>
 			<span className="text-base">{icon}</span>
