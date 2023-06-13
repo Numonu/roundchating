2c1e732bca0347986f5d92ee1a8e65c01541d4f9
Numonu <juan.villegas.dev@gmail.com>
2023-06-13 13:02:00 -0500

    (feat):create room card component

---
+++ src/components/rooms/RoomCard.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { IRoomContextProps, RoomContext } from "../../context/RoomProvider";
+import { Display, ISceneContextProps, SceneContext } from "../../context/SceneProvider";
+import { BiWorld } from "react-icons/bi";
+import { ImEnter } from "react-icons/im";
+
+export default function RoomCard({key , room , }:{key:string | number , room :string}) {
+
+    const { setRoom } = useContext(RoomContext) as IRoomContextProps;
+	const { setScene } = useContext(SceneContext) as ISceneContextProps;
+
+    const goToRoom = (e: string) => {
+		setRoom(e);
+		setScene(Display.CHAT);
+	};
+
+	return (
+		<button
+			className="border-zinc-600 flex justify-between items-center w-full p-4 border-b-2 hover:bg-zinc-950 transition-colors"
+			key={key}
+			onClick={() => goToRoom(room)}
+		>
+            <div className="flex items-center gap-2">
+                <BiWorld className="text-xl"/>
+                {room}
+            </div>
+            <ImEnter className="text-xl"/>
+		</button>
+	);
+}
