b17dda701fbbb284e6f1ad7200963f96807b2cff
Numonu <juan.villegas.dev@gmail.com>
2023-06-13 17:49:36 -0500

    (fix):remove innecesary element

--- src/layout/Chat.tsx
+++ src/layout/Chat.tsx
@@ -4,8 +4,7 @@ import ChatWriter from "../components/chat/ChatWriter";
 export default function Chat() {
 	
 	return (
-		<div className="bg-zinc-800 grid grid-rows-[min-content_1fr_min-content] overflow-hidden">
-			<div className="bg-red-500">Search</div>
+		<div className="bg-zinc-800 grid grid-rows-[1fr_min-content] overflow-hidden">
 			<ChatBody/>
 			<ChatWriter/>
 		</div>
