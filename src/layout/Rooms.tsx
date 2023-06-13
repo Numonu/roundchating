296188c45a116419a00faeb3ccbf8941a6f5444d
Numonu <juan.villegas.dev@gmail.com>
2023-06-13 13:03:35 -0500

    (feat):replace static rooms for dynamic components

--- src/layout/Rooms.tsx
+++ src/layout/Rooms.tsx
@@ -1,30 +1,24 @@
-import { IRoomContextProps, RoomContext } from "../context/RoomProvider";
-import { useContext } from "react";
-import {
-	Display,
-	ISceneContextProps,
-	SceneContext,
-} from "../context/SceneProvider";
+import { BsSearch } from "react-icons/bs";
+import RoomCard from "../components/rooms/RoomCard";
 
 const roomList = ["Global", "Universal", "Local"];
 export default function Rooms() {
-	const { setRoom } = useContext(RoomContext) as IRoomContextProps;
-	const { setScene } = useContext(SceneContext) as ISceneContextProps;
-
-	const goToRoom = (e: string) => {
-		setRoom(e);
-		setScene(Display.CHAT);
-	};
-
 	return (
 		<div className="bg-zinc-900 overflow-hidden">
-			<div>
-				<input type="text" placeholder="Searh Some Room" />
-			</div>
-			<div className="flex flex-col">
-				{roomList.map((e , i) => {
-					return <button key={i} onClick={() => goToRoom(e)}>{e}</button>;
-				})}
+			<div className="border-zinc-600 h-full flex flex-col gap-[2px] border-l-2 border-r-2">
+				<div className="border-zinc-600 w-full p-4 flex items-center gap-2 border-b-2">
+					<BsSearch />
+					<input
+						className="bg-transparent grow outline-none"
+						type="text"
+						placeholder="Searh Some Room"
+					/>
+				</div>
+				<div>
+					{roomList.map((e, i) => {
+						return <RoomCard key={i} room={e} />;
+					})}
+				</div>
 			</div>
 		</div>
 	);
