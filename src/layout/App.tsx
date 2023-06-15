016d7015ff4e6c0e7e516fb1b11f0c8f5ab13c3a
Numonu <juan.villegas.dev@gmail.com>
2023-06-15 16:29:11 -0500

    (feat):add user provider to all app

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,15 +1,17 @@
 import Header from "./Header";
 import SceneProvider from "../context/SceneProvider";
 import FluidGrid from "../components/FluidGrid";
+import UserProvider from "../context/UserProvider";
 
 export default function App() {
-   
 	return (
 		<div className="h-screen grid grid-rows-[min-content_1fr] text-neutral-50">
-			<SceneProvider>
-				<Header />
-				<FluidGrid />
-			</SceneProvider>
+			<UserProvider>
+				<SceneProvider>
+					<Header />
+					<FluidGrid />
+				</SceneProvider>
+			</UserProvider>
 		</div>
 	);
 }
