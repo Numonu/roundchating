0cd73f1ef63fc00c98d2b3a4296e532564c70538
Numonu <juan.villegas.dev@gmail.com>
2023-06-15 16:00:13 -0500

    (feat):add room loading state to toom context

--- src/context/RoomProvider.tsx
+++ src/context/RoomProvider.tsx
@@ -2,14 +2,17 @@ import { ReactNode, createContext , useState} from "react";
 
 export interface IRoomContextProps {
     room : string
+    loadingRoom : boolean
     setRoom : React.Dispatch<React.SetStateAction<string>>
+    setLoadingRoom : React.Dispatch<React.SetStateAction<boolean>>
 }
 export const RoomContext = createContext<IRoomContextProps | null>(null);
 export default function RoomProvider({children}:{children:ReactNode}){
-    const [room , setRoom] = useState<string>("Global");
+    const [room , setRoom] = useState("Global");
+    const [loadingRoom , setLoadingRoom] = useState(false);
 	//
     return (
-        <RoomContext.Provider value={{room , setRoom}}>
+        <RoomContext.Provider value={{room , setRoom , loadingRoom , setLoadingRoom}}>
 			{children}
 		</RoomContext.Provider>
     )
