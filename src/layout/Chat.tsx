05fd9404c2f8504d3cd53e59309852a713f29ca4
Numonu <juan.villegas.dev@gmail.com>
2023-06-15 21:41:46 -0500

    (design):remove bg color in Chat layout

--- src/layout/Chat.tsx
+++ src/layout/Chat.tsx
@@ -4,7 +4,7 @@ import ChatWriter from "../components/chat/ChatWriter";
 export default function Chat() {
 	
 	return (
-		<div className="bg-zinc-800 grid grid-rows-[1fr_min-content] overflow-hidden">
+		<div className="grid grid-rows-[1fr_min-content] overflow-hidden">
 			<ChatBody/>
 			<ChatWriter/>
 		</div>
