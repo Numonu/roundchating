9a01e8284f00a89bfc18b93978089a851bbed1a9
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 09:28:23 -0500

    (design):create design for create rooms

--- src/layout/Rooms.tsx
+++ src/layout/Rooms.tsx
@@ -1,14 +1,15 @@
 import { BsSearch } from "react-icons/bs";
 import RoomCard from "../components/rooms/RoomCard";
 import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
-const roomList = ["Global", "Universal", "Local"];
+const roomList = ["North","South" ,"East", "West"];
 export default function Rooms() {
 	const [search , setSearch] = useState("");
 	return (
 		<div className="bg-zinc-900 overflow-hidden">
 			<div className="border-zinc-600 h-full flex flex-col gap-[2px] border-l-2 border-r-2">
-				<div className="border-zinc-600 w-full p-4 flex items-center gap-2 border-b-2">
+				<div className="border-zinc-600 w-full px-4 py-2 flex items-center gap-3 border-b-2">
 					<BsSearch />
 					<input
 						className="bg-transparent grow outline-none"
@@ -16,6 +17,9 @@ export default function Rooms() {
 						placeholder="Search for a room"
 						onChange={(e) => setSearch(e.target.value)}
 					/>
+					<button className="p-2 rounded-full hover:bg-zinc-600 transition-colors cursor-pointer">
+						<AiOutlinePlus className="text-2xl"/>
+					</button>
 				</div>
 				<div>
 					{roomList.filter(e => e.includes(search)).map((e, i) => {
