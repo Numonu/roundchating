e47e423bec7dae469098e60dc2d703886ae18c5c
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 08:56:34 -0500

    (fix):remove useHeight imports and dependencies

--- src/components/chat/ChatBody.tsx
+++ src/components/chat/ChatBody.tsx
@@ -3,11 +3,9 @@ import { IRoomContextProps, RoomContext } from "../../context/RoomProvider";
 import ChatCard from "./ChatCard";
 import { IUserContextProps, UserContext } from "../../context/UserProvider";
 import ChatSkeleton from "./ChatSkeleton";
-import useHeight from "../../hooks/useHeight";
 import useMesagges from "../../hooks/useMessages";
 
 export default function ChatBody() {
-	const { height, node } = useHeight();
 	const { messages } = useMesagges();
 	//
 	const { user } = useContext(UserContext) as IUserContextProps;
@@ -15,26 +13,26 @@ export default function ChatBody() {
 
 	return (
 		<div
-			className="bg-zinc-800 relative px-2 py-4 flex flex-col gap-2 overflow-y-scroll"
-			ref={node}
-			style={{ height: height }}
+			className="bg-zinc-800 relative"
 		>
-			{(() => {
-				let lastOwner = "";
-				const query = messages.map((e, i, arr) => {
-					lastOwner = e.owner;
-					//
-					const config = {
-						message: e.message,
-						owner: e.owner,
-						ownMessage: user?.displayName === lastOwner,
-						disablePic: arr[i + 1]?.owner === lastOwner,
-					};
-					//
-					return <ChatCard config={config} />;
-				});
-				return query;
-			})()}
+			<div className="absolute inset-0 px-2 py-4 flex flex-col gap-2 overflow-y-scroll">
+				{(() => {
+					let lastOwner = "";
+					const query = messages.map((e, i, arr) => {
+						lastOwner = e.owner;
+						//
+						const config = {
+							message: e.message,
+							owner: e.owner,
+							ownMessage: user?.displayName === lastOwner,
+							disablePic: arr[i + 1]?.owner === lastOwner,
+						};
+						//
+						return <ChatCard config={config} />;
+					});
+					return query;
+				})()}
+			</div>
 			<ChatSkeleton enable={loadingRoom} />
 		</div>
 	);
