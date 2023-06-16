a5f12c19e7cff1ff7b15f40a8876c37608f292e5
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 12:17:01 -0500

    (design):set color text to white in modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,7 @@ interface IModalProps {
 export default function Modal({ children, handleClose }: IModalProps) {
 	return createPortal(
 		<div
-			className="bg-black bg-opacity-70 backdrop-blur-md left-0 top-0 absolute w-screen h-screen flex items-center justify-center"
+			className="bg-black text-neutral-50 bg-opacity-70 backdrop-blur-md left-0 top-0 absolute w-screen h-screen flex items-center justify-center"
 			onClick={handleClose}
 		>
 			{children}
