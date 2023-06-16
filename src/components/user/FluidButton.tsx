50a340f9acaa0a0255f0ee74845834e37a8e866f
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 11:42:21 -0500

    (design):add hover color in fluid btn

--- src/components/user/FluidButton.tsx
+++ src/components/user/FluidButton.tsx
@@ -13,7 +13,7 @@ export default function FluidButton({
 }: IFluidButtonProps) {
 	return (
 		<button
-			className="bg-indigo-600 py-2 rounded-md flex justify-center items-center gap-2 disabled:opacity-50"
+			className="bg-indigo-600 py-2 rounded-md flex justify-center items-center gap-2 disabled:opacity-50 hover:bg-indigo-700 transition-colors"
 			disabled={disabled || false}
 			type={type ?? "submit"}
 		>
