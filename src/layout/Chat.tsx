d3555bc06401e3a06e251f2a01fd347108d565b5
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 10:10:32 -0500

    (design):add bg color

--- src/layout/Chat.tsx
+++ src/layout/Chat.tsx
@@ -1,10 +1,12 @@
 import ChatBody from "../components/chat/ChatBody";
 import ChatWriter from "../components/chat/ChatWriter";
+import RoomTitle from "../components/rooms/RoomTitle";
 
 export default function Chat() {
 	
 	return (
-		<div className="grid grid-rows-[1fr_min-content] overflow-hidden">
+		<div className="bg-zinc-800 grid grid-rows-[min-content_1fr_min-content] overflow-hidden">
+			<RoomTitle/>
 			<ChatBody/>
 			<ChatWriter/>
 		</div>
