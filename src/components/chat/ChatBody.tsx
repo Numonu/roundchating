b3c222444e44768b15b85e8fd6418d5db2b05b19
Numonu <juan.villegas.dev@gmail.com>
2023-06-15 21:41:13 -0500

    (update):set static height in chat body

--- src/components/chat/ChatBody.tsx
+++ src/components/chat/ChatBody.tsx
@@ -1,70 +1,41 @@
-import {
-	onSnapshot,
-	doc,
-	collection,
-	getDocs,
-	orderBy,
-	query,
-} from "firebase/firestore";
-import { db } from "../../firebase/config";
-import { useEffect, useState, useContext } from "react";
+import { useContext } from "react";
 import { IRoomContextProps, RoomContext } from "../../context/RoomProvider";
 import ChatCard from "./ChatCard";
 import { IUserContextProps, UserContext } from "../../context/UserProvider";
 import ChatSkeleton from "./ChatSkeleton";
+import useHeight from "../../hooks/useHeight";
+import useMesagges from "../../hooks/useMessages";
 
-type Message = {
-	owner: string;
-	message: string;
-	timestamp: string;
-};
 export default function ChatBody() {
-	const {user} = useContext(UserContext) as IUserContextProps;
-	const { room , loadingRoom , setLoadingRoom } = useContext(RoomContext) as IRoomContextProps;
-
-	const [messages, setMessages] = useState<Message[]>([]);
-
-	const requestMessages = () => {
-
-		const q = query(collection(db, room), orderBy("timestamp", "asc"));
-		const result: Message[] = [];
-
-		getDocs(q).then((querySnapshot) => {
-			querySnapshot.forEach((doc) => {
-				result.push(doc.data() as Message);
-			});
-			setMessages(result);
-			setLoadingRoom(false);
-		});
-		
-	};
-
-	useEffect(requestMessages, [room]);
-
-	useEffect(() => {
-		const unsub = onSnapshot(doc(db, room, "status"), requestMessages);
-		return unsub;
-	}, [room]);
+	const { height, node } = useHeight();
+	const { messages } = useMesagges();
+	//
+	const { user } = useContext(UserContext) as IUserContextProps;
+	const { loadingRoom } = useContext(RoomContext) as IRoomContextProps;
 
 	return (
-		<div className="relative px-2 py-4 flex flex-col gap-2">
+		<div
+			className="bg-zinc-800 relative px-2 py-4 flex flex-col gap-2 overflow-y-scroll"
+			ref={node}
+			style={{ height: height }}
+		>
 			{(() => {
 				let lastOwner = "";
-				const query = messages.map((e,i,arr) => {
+				const query = messages.map((e, i, arr) => {
 					lastOwner = e.owner;
 					//
 					const config = {
-						message : e.message,
-						owner : e.owner,
-						ownMessage : user?.displayName === lastOwner,
-						disablePic : arr[i + 1]?.owner === lastOwner
-					}
+						message: e.message,
+						owner: e.owner,
+						ownMessage: user?.displayName === lastOwner,
+						disablePic: arr[i + 1]?.owner === lastOwner,
+					};
 					//
-					return <ChatCard config={config}/>;
+					return <ChatCard config={config} />;
 				});
 				return query;
 			})()}
-			<ChatSkeleton enable={loadingRoom}/>
+			<ChatSkeleton enable={loadingRoom} />
 		</div>
 	);
 }
