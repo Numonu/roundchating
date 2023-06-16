f52e47217772c711fbda9fb013d4f8aff14aee1e
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 08:55:03 -0500

    (design):disable pointer events in chat skeleton if is visible

--- src/components/chat/ChatSkeleton.tsx
+++ src/components/chat/ChatSkeleton.tsx
@@ -4,7 +4,7 @@ export default function ChatSkeleton({enable}:{enable:boolean}) {
 	return (
 		<div
 			className={`bg-zinc-800 absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${
-				enable ? "opacity-100" : "opacity-0"
+				enable ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
 			}`}
 		>
 			<AiOutlineLoading3Quarters className="text-3xl animate-spin" />
