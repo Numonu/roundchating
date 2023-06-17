6875227e53ef8d4797af6f22593c28ca1fcaf696
Numonu <juan.villegas.dev@gmail.com>
2023-06-17 12:22:04 -0500

    (design):improve design for secret room popup

--- src/components/rooms/RoomPopup.tsx
+++ src/components/rooms/RoomPopup.tsx
@@ -15,16 +15,19 @@ export default function RoomPopup({ callback }: { callback: () => void }) {
 
 	return (
 		<form
-			className="bg-black w-[90%] p-4 rounded-md max-w-[500px]"
+			className="bg-black w-[90%] p-6 pt-12 rounded-md max-w-[500px]"
 			onClick={(e) => e.stopPropagation()}
 			onSubmit={(e) => {
 				e.preventDefault();
                 connectToNewRoom();
 			}}
 		>
-			<h1 className="text-xl font-medium mb-2">Create or Join a Room</h1>
+			<h1 className="text-2xl font-medium mb-2">Join a secret room</h1>
+			<p className="mb-4">
+				Share the name of the room with your friends
+			</p>
 			<input
-				className="border-zinc-700 bg-transparent w-full py-2 mb-6 border-b-2 outline-none"
+				className="border-zinc-700 bg-transparent w-full py-2 mb-6 border-b-2 outline-none focus:border-white"
 				required
 				type="text"
 				value={roomName}
@@ -32,12 +35,9 @@ export default function RoomPopup({ callback }: { callback: () => void }) {
 				onChange={(e) => setRoomName(e.target.value)}
 			/>
 			<div
-				className="w-full grid grid-cols-1 gap-2 md:grid-cols-2"
+				className="w-full flex justify-end gap-2"
 			>
-                <button className="bg-indigo-600 py-2 rounded-md hover:bg-indigo-700 transition-colors">
-					Create
-				</button>
-				<button className="bg-zinc-800 py-2 rounded-md hover:bg-zinc-900 transition-colors">
+                <button className="bg-indigo-600 w-1/3 py-2 font-medium rounded-md hover:bg-indigo-700 transition-colors">
 					Join
 				</button>
 			</div>
