ef67d44687677b18d1ef6189af9b5b89c89d53bf
Numonu <juan.villegas.dev@gmail.com>
2023-06-17 11:28:16 -0500

    (fix):remove delay on render messages and add fallback for errors

--- src/hooks/useMessages.tsx
+++ src/hooks/useMessages.tsx
@@ -21,12 +21,11 @@ export default function useMesagges(){
 			querySnapshot.forEach((doc) => {
 				result.push(doc.data() as Message);
 			});
-			new Promise(res => setTimeout(res , 1000)).then(() => {
-				setMessages(result);
-				setLoadingRoom(false);
-			})
-		});
-		
+			setMessages(result);
+			setLoadingRoom(false);
+		}).catch(() => {
+			requestMessages();
+		})	
 	} , [room , setLoadingRoom]);
 
 	useEffect(() => {
