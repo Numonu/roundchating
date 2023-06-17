d2f1842a7d3449d6e71cebaadd0b271e54a3fc58
Numonu <juan.villegas.dev@gmail.com>
2023-06-17 12:07:12 -0500

    (feat):message downloading is now paginated

--- src/hooks/useMessages.tsx
+++ src/hooks/useMessages.tsx
@@ -1,6 +1,6 @@
 import { useState , useContext , useEffect , useCallback } from "react";
 import { IRoomContextProps, RoomContext } from "../context/RoomProvider";
-import { collection, doc, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, doc, getDocs, onSnapshot, orderBy, query, startAfter } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 type Message = {
@@ -12,29 +12,36 @@ type Message = {
 export default function useMesagges(){
 	const [messages, setMessages] = useState<Message[]>([]);
 	const { room , setLoadingRoom } = useContext(RoomContext) as IRoomContextProps;
+	const [startQueryAt, setStartQueryAt] = useState(0);
 
     const requestMessages = useCallback(() => {
 
-		const q = query(collection(db, room), orderBy("timestamp", "asc"));
+		const q = query(collection(db, room), orderBy("timestamp", "asc") , startAfter(startQueryAt));
 		const result: Message[] = [];
 
 		getDocs(q).then((querySnapshot) => {
 			querySnapshot.forEach((doc) => {
 				result.push(doc.data() as Message);
 			});
+			setStartQueryAt(result.length);
+			//
 			setMessages(result);
 			setLoadingRoom(false);
+			//
 		}).catch(() => {
 			requestMessages();
 		})	
-	} , [room , setLoadingRoom]);
+	} , [room , startQueryAt , setLoadingRoom]);
 
 	useEffect(() => {
         const unsub = onSnapshot(doc(db, room, "status"), requestMessages);
 		return unsub;
 	}, [requestMessages, room]);
     
-    useEffect(requestMessages, [room, requestMessages]);
+    useEffect(() => {
+		requestMessages();
+		setStartQueryAt(0);
+	}, [room, requestMessages]);
 
     return{
         messages
