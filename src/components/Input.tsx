1d4e280fb8f57beae9e2525c697b6145a103783c
Numonu <juan.villegas.dev@gmail.com>
2023-06-16 21:52:32 -0500

    (feat):the input component now accept pattern and title props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,10 +3,12 @@ interface IInputProps {
     icon? : ReactNode,
     id : string,
     type: string,
+	pattern? : string,
+	title?:string,
     placeholder : string,
     onChange : (e:string) => void;
 }
-export default function Input({ icon , id , type , placeholder , onChange}:IInputProps) {
+export default function Input({ icon , id , type , placeholder , pattern, title, onChange}:IInputProps) {
 	return (
 		<label className="bg-zinc-800 px-2 py-3 flex items-center gap-3 text-xl rounded-md border-2 border-transparent focus-within:border-neutral-50 transition-colors" htmlFor={id}>
             <span className="text-3xl">
@@ -17,6 +19,8 @@ export default function Input({ icon , id , type , placeholder , onChange}:IInpu
 				required
 				id={id}
 				type={type}
+				title={title}
+				pattern={pattern}
 				placeholder={placeholder}
 				onChange={(e) => onChange(e.target.value)}
 			/>
